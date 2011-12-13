838f992b495ccd02553d6bd290b4907b51bf880a
David Robert <davidrobert@gmail.com>
2011-12-13 20:01:20 -0200

    Mostra o nome do estudante ao clicar na foto

--- StudentContacts/src/br/com/while42/activity/GalleryStudents.java
+++ StudentContacts/src/br/com/while42/activity/GalleryStudents.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Gallery;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.SpinnerAdapter;
 import br.com.while42.adapter.GalleryStudentAdapter;
 import br.com.while42.model.Student;
 import br.com.while42.persist.StudentDAO;
@@ -37,6 +42,15 @@ public class GalleryStudents extends Activity {
 		adapter = new GalleryStudentAdapter(this, students);
 		
 		gallery = (Gallery) findViewById(R.id_galery.galleryPhoto);     
-		gallery.setAdapter(adapter);		
+		gallery.setAdapter(adapter);
+		
+		gallery.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View v, int posicao, long id) {
+				// Mostra o nome do estudante ao tocar na foto
+				Toast.makeText(GalleryStudents.this, students.get(posicao).toString(), Toast.LENGTH_SHORT).show();			
+			}
+			
+		});
 	}
 }
