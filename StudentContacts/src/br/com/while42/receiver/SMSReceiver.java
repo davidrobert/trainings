9d555984d80938c823908abc2dd10d2726452637
David Robert <davidrobert@gmail.com>
2011-12-13 23:31:54 -0200

    pequenas correcoes

--- StudentContacts/src/br/com/while42/receiver/SMSReceiver.java
+++ StudentContacts/src/br/com/while42/receiver/SMSReceiver.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.Toast;
 import br.com.while42.R;
 import br.com.while42.model.Student;
@@ -16,21 +17,28 @@ public class SMSReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Bundle bundle = intent.getExtras();
-		Object[] messages = (Object[]) bundle.getParcelableArray("pdus");
+		Object[] messages = (Object[]) bundle.get("pdus");
 		
 		SmsMessage smsMessage[] = new SmsMessage[messages.length];
+		Log.i("DEBUG>>", Integer.toString(messages.length));
 		
 		StudentDAO dao = new StudentDAO(context);
 		
 		for (int i = 0; i < messages.length; i++) {
-			smsMessage[i] = SmsMessage.createFromPdu((byte[]) messages[i]);			
+			smsMessage[i] = SmsMessage.createFromPdu((byte[]) messages[i]);
+			Log.i("DEBUG", smsMessage[i].toString());
 			
-			Student student = dao.findByPhone(smsMessage[i].toString());
+			String phone = smsMessage[i].getDisplayOriginatingAddress();
+			Student student = dao.findByPhone(phone);
+			Log.i("-->", phone);
 			if (student != null) {				
 				Toast.makeText(context, student.getName(), Toast.LENGTH_LONG);
-				MediaPlayer player = MediaPlayer.create(context, R.raw.msg);
-				player.start();
+				
+			} else {
+				Toast.makeText(context, phone, Toast.LENGTH_LONG);
 			}
+			MediaPlayer player = MediaPlayer.create(context, R.raw.msg);
+			player.start();
 		}
 
 		dao.close();		
