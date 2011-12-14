9c0cd492124eec2ae5500a736297835f28c8c350
David Robert <davidrobert@gmail.com>
2011-12-14 19:30:41 -0200

    Pequena modificacao no tratamento do SMS recebido

--- StudentContacts/src/br/com/while42/receiver/SMSReceiver.java
+++ StudentContacts/src/br/com/while42/receiver/SMSReceiver.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
-import android.util.Log;
 import android.widget.Toast;
 import br.com.while42.R;
 import br.com.while42.model.Student;
@@ -20,22 +19,19 @@ public class SMSReceiver extends BroadcastReceiver {
 		Object[] messages = (Object[]) bundle.get("pdus");
 		
 		SmsMessage smsMessage[] = new SmsMessage[messages.length];
-		Log.i("DEBUG>>", Integer.toString(messages.length));
 		
 		StudentDAO dao = new StudentDAO(context);
 		
 		for (int i = 0; i < messages.length; i++) {
 			smsMessage[i] = SmsMessage.createFromPdu((byte[]) messages[i]);
-			Log.i("DEBUG", smsMessage[i].toString());
 			
 			String phone = smsMessage[i].getDisplayOriginatingAddress();
 			Student student = dao.findByPhone(phone);
-			Log.i("-->", phone);
 			if (student != null) {				
-				Toast.makeText(context, student.getName(), Toast.LENGTH_LONG);
+				Toast.makeText(context, "Recebido SMS de " + student.getName(), Toast.LENGTH_LONG).show();
 				
 			} else {
-				Toast.makeText(context, phone, Toast.LENGTH_LONG);
+				Toast.makeText(context, "Recebido SMS de " + phone, Toast.LENGTH_LONG).show();
 			}
 			MediaPlayer player = MediaPlayer.create(context, R.raw.msg);
 			player.start();
