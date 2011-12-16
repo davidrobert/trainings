74de11f914557b94020ba663e16f3bdd22959714
David Robert <davidrobert@gmail.com>
2011-12-15 22:54:01 -0200

    Inicio da implementacao das preferencias

---
+++ StudentContacts/src/br/com/while42/activity/Preferences.java
@@ -0,0 +1,20 @@
+package br.com.while42.activity;
+
+import br.com.while42.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+
+public class Preferences extends Activity {
+
+	private CheckBox check = null;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		check = (CheckBox) findViewById(R.id_preferences.checkBoxOrder);
+	}
+	
+	
+}
