50c16a5b5536d4367addaed8b498472043b25384
David Robert <davidrobert@gmail.com>
2011-12-17 14:17:08 -0200

    Add TODO

--- src/br/com/while42/studantcontacts/activity/Preferences.java
+++ src/br/com/while42/studantcontacts/activity/Preferences.java
@@ -7,13 +7,15 @@ import br.com.while42.studantcontacts.R;
 
 public class Preferences extends Activity {
 
-	private CheckBox check = null;
+	// private CheckBox check = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		check = (CheckBox) findViewById(R.id_preferences.checkBoxOrder);
+		// check = (CheckBox) findViewById(R.id_preferences.checkBoxOrder);
+		
+		// TODO: Falta terminar a implementação
 	}
 	
 	
