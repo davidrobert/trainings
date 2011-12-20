12ffecf33c9b1a6abddcc9b0d28639ba4e91ef6e
davidrobert <davidrobert@gmail.com>
2011-12-19 22:12:13 -0200

    Add class test

---
+++ src/main/java/br/com/while42/argentum/model/TestCandlestickFactory.java
@@ -0,0 +1,29 @@
+package br.com.while42.argentum.model;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+public class TestCandlestickFactory {
+
+	public static void main(String[] args) {
+
+		Calendar hoje = Calendar.getInstance();
+
+		Operation op1 = new Operation(40.5, 100, hoje);
+		Operation op2 = new Operation(45.0, 100, hoje);
+		Operation op3 = new Operation(39.8, 100, hoje);
+		Operation op4 = new Operation(42.3, 100, hoje);
+
+		List<Operation> operations = Arrays.asList(op1, op2, op3, op4);
+		
+		CandleStickFactory factory = new CandleStickFactory();
+		Candlestick candle = factory.buidCandleToDate(hoje, operations);
+		
+		System.out.println(candle.getFirst());
+		System.out.println(candle.getLast());
+		System.out.println(candle.getMin());
+		System.out.println(candle.getMax());
+		System.out.println(candle.getVolume());
+	}
+}
