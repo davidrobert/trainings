5397e1f54f4c3b82ed044d48a299dad34985d9d6
davidrobert <davidrobert@gmail.com>
2011-12-22 20:25:45 -0200

    Test time series

---
+++ src/test/java/br/com/while42/argentum/model/TimeSeriesTest.java
@@ -0,0 +1,14 @@
+package br.com.while42.argentum.model;
+
+import org.junit.Test;
+
+public class TimeSeriesTest {
+	
+	@Test
+	public void shouldNotAccepttNull() {
+		new TimeSeries(null);
+	}
+	
+	
+	
+}
