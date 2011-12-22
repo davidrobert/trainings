0bb0b894909268fec357ae2e506a9622d37d4965
davidrobert <davidrobert@gmail.com>
2011-12-22 20:24:32 -0200

    Add time series

---
+++ src/main/java/br/com/while42/argentum/indicators/SimpleMovingAverage.java
@@ -0,0 +1,12 @@
+package br.com.while42.argentum.indicators;
+
+import br.com.while42.argentum.model.TimeSeries;
+
+public class SimpleMovingAverage {
+	
+	public double calcule(int position, TimeSeries serie) {
+		return 0;
+	}
+	
+	
+}
---
+++ src/test/java/br/com/while42/argentum/indicators/SimpleMovingAverageTest.java
@@ -0,0 +1,12 @@
+package br.com.while42.argentum.indicators;
+
+import br.com.while42.argentum.model.TimeSeries;
+
+public class SimpleMovingAverageTest {
+	
+	
+	private TimeSeries buildTimeSerie(double ... values) {
+		
+		return null;
+	}
+}
