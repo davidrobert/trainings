14e35f2eecacb351e3be5627625c9a725338bc4a
davidrobert <davidrobert@gmail.com>
2011-12-23 21:53:02 -0200

    Interface improvements

--- src/main/java/br/com/while42/argentum/ui/ArgentumUI.java
+++ src/main/java/br/com/while42/argentum/ui/ArgentumUI.java
@@ -1,7 +1,7 @@
 package br.com.while42.argentum.ui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -22,6 +22,14 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.LineBorder;
 
+import br.com.while42.argentum.graphics.Chart;
+import br.com.while42.argentum.indicators.FirstValueIndicator;
+import br.com.while42.argentum.indicators.LastValueIndicator;
+import br.com.while42.argentum.indicators.SimpleMovingAverage;
+import br.com.while42.argentum.indicators.WeightedMovingAverage;
+import br.com.while42.argentum.model.Candle;
+import br.com.while42.argentum.model.CandleFactory;
+import br.com.while42.argentum.model.TimeSerie;
 import br.com.while42.argentum.model.Trade;
 
 public class ArgentumUI {
@@ -33,9 +41,9 @@ public class ArgentumUI {
 	private JPanel panelButtons;
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(new Locale("pt", "BR"));
-		
+
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException e) {
@@ -53,17 +61,17 @@ public class ArgentumUI {
 		}
 		new ArgentumUI().showWindow();
 	}
-	
+
 	private void showWindow() {
 		buildWindow();
-		buildMainPanel();	
+		buildMainPanel();
 		builTitle();
 		buildTabs();
-		buildPainelButtons();			
+		buildPainelButtons();
 		buildLoadButton();
 		buildExitButton();
 		buildTable();
-		showScreen();		
+		showScreen();
 	}
 
 	private void builTitle() {
@@ -71,92 +79,105 @@ public class ArgentumUI {
 		title.setFont(new Font("Verdana", Font.BOLD, 25));
 		title.setForeground(new Color(50, 50, 100));
 		title.setHorizontalAlignment(SwingConstants.CENTER);
-		mainPanel.add(title);
+		mainPanel.add(title, BorderLayout.NORTH);
 	}
-	
+
 	private void buildWindow() {
-		window = new JFrame("Argentum");		
+		window = new JFrame("Argentum");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
+
 	private void buildMainPanel() {
 		mainPanel = new JPanel();
-		
+
 		// mainPanel.setLayout(new FlowLayout());
 		// mainPanel.setLayout(new GridLayout());
-		// mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 		// mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
-		
+
 		window.add(mainPanel);
 	}
-	
+
 	private void buildTable() {
 		table = new JTable();
-		
+
 		table.setBorder(new LineBorder(Color.black));
 		table.setGridColor(Color.black);
-		table.setShowGrid(true);		
-		
+		table.setShowGrid(true);
+
 		JScrollPane scroll = new JScrollPane();
 		scroll.getViewport().setBorder(null);
 		scroll.getViewport().add(table);
-		scroll.setSize(200, 200);		
-		
+		scroll.setSize(200, 200);
+
 		tabs.setComponentAt(0, scroll);
-		//mainPanel.add(scroll);
+		// mainPanel.add(scroll);
 	}
-	
+
 	private void buildPainelButtons() {
 		panelButtons = new JPanel(new GridLayout());
-		mainPanel.add(panelButtons);		
+		mainPanel.add(panelButtons);
 	}
 
 	private void buildLoadButton() {
 		JButton bLoad = new JButton("Load XML");
-		
+
 		bLoad.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				List<Trade> trades = new ChooseXML().selectFile();
-				TradeTableModel tradeTable = new TradeTableModel(trades);
-				table.setModel(tradeTable);
+				loadData();				
 			}
 		});
-				
+
 		panelButtons.add(bLoad);
 	}
-	
+
 	private void buildExitButton() {
 		JButton bExit = new JButton("Exit");
-		
+
 		bExit.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
 		});
-		
+
 		panelButtons.add(bExit);
 	}
-	
+
 	private void showScreen() {
-		window.pack();				
+		window.pack();
 		window.setVisible(true);
-		window.setSize(530, 560);
+		window.setSize(700, 600);
 	}
-	
+
 	private void buildTabs() {
 		tabs = new JTabbedPane();
 		tabs.addTab("Tabela de Negocios", null);
 		tabs.addTab("Gráfico", null);
-		
+
 		mainPanel.add(tabs);
 	}
-	
+
 	private void loadData() {
 		List<Trade> trades = new ChooseXML().selectFile();
-		
+
 		TradeTableModel tableModel = new TradeTableModel(trades);
+		this.table.setModel(tableModel);
+
+		CandleFactory factory = new CandleFactory();
+		List<Candle> candles = factory.buildCandle(trades);
+		TimeSerie serie = new TimeSerie(candles);
+
+		Chart chart = new Chart().withTimeSerie(serie).start(2).end(serie.getTotal() - 1)
+				.title("Média Móvel Simples")
+				.withIndicator(new WeightedMovingAverage(new FirstValueIndicator()))
+				.withIndicator(new SimpleMovingAverage(new FirstValueIndicator()))
+				.withIndicator(new WeightedMovingAverage(new LastValueIndicator()))
+				.withIndicator(new SimpleMovingAverage(new LastValueIndicator()));
+		
+		JPanel panel = chart.getPanel();
+		this.tabs.setComponentAt(1, panel);
 	}
 
 }
