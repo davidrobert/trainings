9163944ae5976a63d1bee95c77b1b3f47f57121d
David Robert <davidrobert@gmail.com>
2012-01-06 14:15:53 -0200

    Removing unnecessary files

--- agenda/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Agenda::Application.config.secret_token = 'cd0ad6219826feb7cbd23b0ddcef48171bd2931b5b11964869d0f2427ee7c8de077dfe8e41e3ad7217b6b9404810c888655862a221f9c5f25ddff4eb489b6ac3'
