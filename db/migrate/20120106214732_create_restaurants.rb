025e12705de084ba56870340ed4b682a5431aee0
David Robert <davidrobert@gmail.com>
2012-01-06 20:23:20 -0200

    Add specialty restaurant

--- db/migrate/20120106214732_create_restaurants.rb
+++ db/migrate/20120106214732_create_restaurants.rb
@@ -1,7 +1,8 @@
 class CreateRestaurants < ActiveRecord::Migration
   def self.up
     create_table :restaurants do |t|
-
+			t.string :name, :limit => 80
+			t.string :address
       t.timestamps
     end
   end
