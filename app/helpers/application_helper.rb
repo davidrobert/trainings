bbbd970bfbf6c6021382c7b6a00d7d36dafd9851
David Robert <davidrobert@gmail.com>
2012-01-10 22:17:39 -0200

    Add helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def main_menu
+		menu = %w(client qualification restaurant)
+		main_menu = "<ul>"
+		menu.each do |item|
+			main_menu << "<li>" + link_to(item, :controller => item.pluralize) + "</li>"
+		end
+		main_menu << "</ul>"
+		raw main_menu
+	end
 end
