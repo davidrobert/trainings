3371c0ad2aa81948b6a477bf3f80a36ee56fe2a6
David Robert <davidrobert@gmail.com>
2012-01-19 21:38:47 -0200

    [RR-75] Restfull - Restifulie

--- RR-75/cardealer/app/helpers/application_helper.rb
+++ RR-75/cardealer/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def hour_now
+		uri = 'http://developer.yahooapis.com/TimeService/V1/getTime'
+		response = Restfulie.at(uri).get(:appid => 'YahooDemo').resource
+		yahoo_time = response['Result']['Timestamp'].to_i
+		Time.at(yahoo_time).strftime("%d/%m/%Y %H:%M")
+	end
 end
