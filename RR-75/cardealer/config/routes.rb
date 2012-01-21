6524b8f2b6c0c21b2dd6210f619f55da9991f0d3
David Robert <davidrobert@gmail.com>
2012-01-20 22:29:52 -0200

    removal of duplicate rows

--- RR-75/cardealer/config/routes.rb
+++ RR-75/cardealer/config/routes.rb
@@ -4,7 +4,6 @@ Cardealer::Application.routes.draw do
   resources :cars
 
 	root to: "cars#index"
-	resources "cars"		
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
